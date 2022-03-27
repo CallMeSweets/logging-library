36ba3ff9cf63aae7364568282d7bd6c1e578fc00
aleokr <aleksandra.okrutny1@gmail.com>
2022-03-27 23:01:17 +0200

    add yaml-cpp library

--- LoggerLibrary/main.cpp
+++ LoggerLibrary/main.cpp
@@ -1,6 +1,7 @@
 #include "logger/Logger.h"
 #include "logger/service/elastic/ElasticLoggingService.h"
 #include "logger/service/file/FileLoggingService.h"
+#include "yaml-cpp/yaml.h"
 
 #include <thread>
 
@@ -14,18 +15,28 @@ int main() {
 
 //    LoggingService* service = new FileLoggingService{"logg.txt"};
 //    Logger::enableFileService(service);
-    LoggingService* elService = new ElasticLoggingService{"logging-03-2022", "localhost", "9200"};
-    Logger::enableFileService(elService);
+//    LoggingService* elService = new ElasticLoggingService{"logging-03-2022", "localhost", "9200"};
+//    Logger::enableFileService(elService);
 
-    std::thread threads[10];
+//    std::thread threads[10];
+//
+//    for (int i = 0; i < 10; ++i) {
+//        threads[i] = std::thread(logzz, i);
+//    }
+//
+//    for (int i = 0; i < 10; ++i) {
+//        threads[i].join();
+//    }
 
-    for (int i = 0; i < 10; ++i) {
-        threads[i] = std::thread(logzz, i);
+    YAML::Node doc = YAML::Load("{name: Brewers, city: Milwaukee}");
+//    if (node["aaaaaa"]) {
+//        const std::string password = node["aaaaaa"].as<std::string>();
+//        std::cout << password << "\n";
+//    }
+    if (doc["name"]) {
+        std::cout << doc["name"].as<std::string>() << "\n";
     }
-
-    for (int i = 0; i < 10; ++i) {
-        threads[i].join();
-    }
-
+//    doc["aaa"] = 5;
+    std::cout << "aa";
     return 0;
 }
